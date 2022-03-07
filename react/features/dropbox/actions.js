30018d9e962e49fbfbd8290e68d7aaa08f479ceb
Norbert Preining <norbert@preining.info>
2022-03-08 01:31:58 +0900

    Hard code NC redirect into Dropbox

--- react/features/dropbox/actions.js
+++ react/features/dropbox/actions.js
@@ -14,6 +14,8 @@ export function authorizeDropbox() {
         const { locationURL } = state['features/base/connection'];
         const { dropbox = {} } = state['features/base/config'];
 
+        dropbox.redirectURI = `${locationURL.origin}/static/oauth.html`;
+
         // By default we use the static page on the main domain for redirection.
         // So we need to setup only one redirect URI in dropbox app
         // configuration (not multiple for all the tenants).
@@ -22,13 +24,68 @@ export function authorizeDropbox() {
         const redirectURI
             = dropbox.redirectURI || `${locationURL.origin}/static/oauth.html`;
 
-        _authorizeDropbox(dropbox.appKey, redirectURI)
-            .then(
-                ({ token, rToken, expireDate }) => {
-                    dispatch(updateDropboxToken(token, rToken, expireDate));
+        const config = {
+            oAuthURL: 'https://cloud.preining.info/index.php/apps/oauth2/authorize',
+            tokenURL: 'https://cloud.preining.info/index.php/apps/oauth2/api/v1/token',
+            clientID: 'xcgU61E19lGTPO2JEgxGU4SURHhNgWwjDZ45tRRTBQ6KY26gslJ4CFlejimZsnNT',
+            secret: 'kFNc3NyvG8ozztumzz112zsopf0CaJc7kBgzzfXgXK4DsnL9gPvEaKuspfxS5Msq',
+            redirectURL: 'https://meet.preining.info/static/oauth.html'
+        }
+
+        const oauthLoginChannel = new BroadcastChannel('oauthLoginChannel')
+        oauthLoginChannel.onmessage = async e => {
+            const code = e.data
+            try {
+                console.log('return the auth code')
+
+                const params = {
+                    grant_type: "authorization_code",
+                    client_id: config.clientID,
+                    client_secret: config.secret,
+                    redirect_uri: config.redirectURL,
+                    code: code,
+                }
+
+                const response = await fetch(config.tokenURL,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json;charset=UTF-8",
+                            "Origin": `${locationURL.origin}`,
+                            "Referer": `${locationURL.origin}`
+                        },
+                        body: JSON.stringify(params)
+                    }).then(response => {
+                    return !!response ? response.json() : undefined;
+                }).catch(e => {
+                    // TODO: CORS error handling
                 })
-            .catch(error => logger.log('Cannot authorize dropbox', error));
+                console.log(response)
+
+            } catch (e) {
+                console.error('Failed to authenticate with received token', code, e)
+                //ui.error = (e as Error).message
+            }
+            //ui.loginLoading = false
+        }
+
+        // Open popup window with OAuth provider page
+        const width = 600, height = 600
+        const left = window.innerWidth / 2 - width / 2
+        const top = window.innerHeight / 2 - height / 2
+
+
+        const param = `response_type=code&client_id=${config.clientID}&redirect_uri=${config.redirectURL}`
+        window.open(
+            `${config.oAuthURL}?${param}`,
+            '',
+            `toolbar=no, location=no, directories=no, status=no, menubar=no,
+    scrollbars=no, resizable=no, copyhistory=no, width=${width},
+    height=${height}, top=${top}, left=${left}`,
+        )
+
     };
+
 }
 
 /**
@@ -52,3 +109,4 @@ export function updateDropboxToken(token: string, rToken: string, expireDate: nu
         expireDate
     };
 }
+
